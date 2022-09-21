21e53a85daf3e3b5222e259b2eea070de39f5967
Simon Timms <stimms@gmail.com>
2022-09-21 15:47:04 -0600

    No documentation

--- APIDemo/Program.cs
+++ APIDemo/Program.cs
@@ -4,11 +4,6 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.Configure<JsonOptions>(options =>
 {
     options.SerializerOptions.Converters.Add(new APIDemo.DateTimeConverter());
